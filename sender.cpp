8e1381d62573f8b74ca91b52d6f50cccfc0c6c85
Austin (Austin) Campbell <cama3780@36magneto.cs.uleth.ca>
2021-03-24 20:40:18 -0600

    Sender now sends messages in 512 payloads

--- sender.cpp
+++ sender.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <vector>
+#include <bitset>
 #include <string.h>
 #include <fstream>
 #include <sys/types.h>
@@ -10,9 +12,14 @@
 #include <arpa/inet.h>
 #include "Header.h"
 
+#include <sstream>
+
 #define MAXLINE 1024
 
+const int PAYLOAD = 512;
+
 std::string readFromFile(std::string fileName);
+std::vector<std::string> textTo512Bin(std::string text);
 
 int main(int argc, char* argv[]) {
 
@@ -20,8 +27,9 @@ int main(int argc, char* argv[]) {
    int port; //Port being sent to
    int sockfd; // File descriptor of socket
    struct sockaddr_in servaddr, cliaddr;
-   char *message = "Message from sender";
+   std::string message = ""; //char *message = "Message from sender";
    char buffer[MAXLINE];
+   std::vector<std::string> binMessage;
 
    // If there is an incorrect amount of inputs
    if (argc != 5 && argc != 3) {
@@ -45,6 +53,8 @@ int main(int argc, char* argv[]) {
    }
 
    std::cout <<"host: " <<host <<" " <<"\nport: " <<port <<std::endl;
+   
+   binMessage = textTo512Bin(message);
 
    // Creates the unbounded socket in IPv4
    // socket(int domain, int type, int protocol)
@@ -73,6 +83,16 @@ int main(int argc, char* argv[]) {
 
    len = sizeof(cliaddr);
 
+   for(int i = 0; i < binMessage.size(); i++) {
+
+      std::cout <<"i: " <<i <<std::endl;
+      char *send = (char *)binMessage[i].c_str();
+
+      //Send message to socket
+      // sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
+      sendto(sockfd, (const char *)send, strlen(send), MSG_CONFIRM,
+	     (const struct sockaddr *)&cliaddr, len);
+
    // Recieve messge from socket
    // recvfrom(int sockfd, void *buf, size_t len, int flags,
    //            struct sockaddr *src_addr, socklen_t *addrlen);
@@ -82,11 +102,8 @@ int main(int argc, char* argv[]) {
    buffer[n] = '\0';
 
    std::cout <<"Message from receiver:\n" <<buffer <<std::endl;
-
-   // Send message to socket
-   // sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
-   sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM,
-	  (const struct sockaddr *)&cliaddr, len);
+      
+   }
 
    std::cout <<"Sent message to " <<host <<std::endl;
 
@@ -99,4 +116,30 @@ std::string readFromFile(std::string fileName) {
                        (std::istreambuf_iterator<char>()    ) );
 
    return content;
+}
+
+// Converts text to 512 bit chunks
+std::vector<std::string> textTo512Bin(std::string text) {
+   // Vector containing 512 chunks
+   std::vector<std::string> final;
+   // Text as binary
+   std::string binary = "";
+
+   // Converts text to binary
+   // Saves it in binary
+   for (int i = 0; i < text.size(); i++)
+      binary.append(std::bitset<8>(text[i]).to_string());
+
+   // Splits binary into 512 and pushes it into final
+   for (int i = 0; i <= binary.size()/PAYLOAD; i++) {
+      std::string temp = "";
+
+      // makes sure we arn't going over the file length
+      for(int j = 0; j < PAYLOAD && j + (i * PAYLOAD) < binary.size(); j++)
+	 temp += binary[j + (i * PAYLOAD)];
+
+      final.push_back(temp);
+   }
+   
+   return final;
 }
